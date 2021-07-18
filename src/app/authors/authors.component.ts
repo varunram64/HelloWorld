01eda388f830449a1a4c31e7481decbf8353b588
Varun Kalidindi <varunram64@gmail.com>
2021-07-18 18:46:18 +0530

    Another way to do Excercise 2

--- src/app/authors/authors.component.ts
+++ src/app/authors/authors.component.ts
@@ -2,14 +2,8 @@ import { AuthorsService } from './../authors.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-authors',
-  template: `<h2>{{ title }}</h2>
-             <ul>
-                <li *ngFor="let author of authors">
-                  {{ author }}
-                </li>
-             </ul>
-            `,
+  selector: 'authors',
+  templateUrl: './authors.component.html',
   styleUrls: ['./authors.component.css']
 })
 

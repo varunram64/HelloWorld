bf8727c0863801e42cb6f1eba26af1671d87c0c9
Varun Kalidindi <varunram64@gmail.com>
2021-07-18 18:21:23 +0530

    Services

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CoursesService } from './courses.service';
 
 @Component({
     selector: 'courses',    // <courses> - 'courses'
@@ -16,5 +17,10 @@ import { Component } from '@angular/core';
 
 export class CoursesComponent {
     title = 'List of Courses';
-    courses = ["Course1", "Course2", "Course3"];
+    // courses = ["Course1", "Course2", "Course3"];
+    courses;
+
+    constructor(service: CoursesService) {
+        this.courses = service.getCourses();
+    }
 }

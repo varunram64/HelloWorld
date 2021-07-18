e060e6592eb6d0be18d657e69f1dfd743cb80ec7
Varun Kalidindi <varunram64@gmail.com>
2021-07-18 17:39:53 +0530

    Templates

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,9 +4,13 @@ import { Component } from '@angular/core';
     selector: 'courses',    // <courses> - 'courses'
                             // <div id='courses'.. - '#courses'
                             // <div class='courses'.. - '.courses'
-    template: '<h2>Courses</h2>'
+    template: '<h2>{{ getTitle() }}</h2>'
 })
 
 export class CoursesComponent {
+    title = 'List of Courses';
 
+    getTitle() {
+        return this.title;
+    }
 }

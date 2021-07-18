6d69c268080ec9201e82e9938c733d3ab49a1eee
Varun Kalidindi <varunram64@gmail.com>
2021-07-18 17:25:30 +0530

    New Component created and registered as Module.

---
+++ src/app/courses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'courses',    // <courses> - 'courses'
+                            // <div id='courses'.. - '#courses'
+                            // <div class='courses'.. - '.courses'
+    template: '<h2>Courses</h2>'
+})
+
+export class CoursesComponent {
+
+}

7d79668460ce4dcc89e9e409fec0b2e4749a0b46
Varun Kalidindi <varunram64@gmail.com>
2021-07-20 15:32:38 +0530

    Styles

--- src/app/favourites/favourites.component.ts
+++ src/app/favourites/favourites.component.ts
@@ -3,7 +3,14 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'favourites',
   templateUrl: './favourites.component.html',
-  styleUrls: ['./favourites.component.css'],
+  styleUrls: ['./favourites.component.css'], //We can use both file and parameters
+  styles: [
+  `
+    .bi {
+      color: green;
+    }
+  ` 
+  ], // If this is last then this style is applied
   inputs: ['title'] // Disadvantage to this approach of magic string is 
                     //whenever we change name we have to change here as well
 })
@@ -11,9 +18,6 @@ export class FavouritesComponent {
   @Input('isFavorite') isSelected = false;
   title = '';
   @Output('change') click = new EventEmitter();
-
-  constructor() { 
-  }
 
   onFavouriteClick() {
     this.isSelected = !this.isSelected;

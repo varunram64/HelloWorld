a3950cabc492e1cfcebb41a5438c5ea4e4dd55dc
Varun Kalidindi <varunram64@gmail.com>
2021-07-20 11:20:18 +0530

    Input properties

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-course',
+  selector: 'course',
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
+  imageURL = "";
+  post = {
+    isFavorite: true,
+    title: "Course"
+  };
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSaveClick() {
+
   }
 
 }
--- src/app/favourites/favourites.component.ts
+++ src/app/favourites/favourites.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'favourites',
   templateUrl: './favourites.component.html',
-  styleUrls: ['./favourites.component.css']
+  styleUrls: ['./favourites.component.css'],
+  inputs: ['title'] // Disadvantage to this approach of magic string is 
+                    //whenever we change name we have to change here as well
 })
 export class FavouritesComponent {
-  isFavorite = false;
+  @Input() isFavorite = false;
+  title = '';
 
   constructor() { 
   }
